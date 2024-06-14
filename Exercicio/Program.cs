309d5f8fc2f0be2c0ba64f5034bf4c1b643bb003
Lucas Kenji <lucasknishida@gmail.com>
2024-06-14 17:40:26 -0300

    first commit

---
+++ Exercicio/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exericio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            bool ehPrimo = true;
+
+            if(num <= 1)
+            {
+                ehPrimo = false;
+            }
+            else
+            {
+                for(int i = 2; i <= num / 2; i ++)
+                {
+                    int divisao = num / i;
+                    int resto = num - divisao * i;
+                    if(resto == 0)
+                    {
+                        ehPrimo = false;
+                    }
+                }
+            }
+
+            if(ehPrimo)
+            {
+            Console.WriteLine("É primo");
+            }
+            else
+            {
+                Console.WriteLine("Não é primo");
+            }
+        }
+    }
+}
